0e9f6d2c7e27a5a4d9b687b7d2dd3b9582055a97
Olena Zamkova <olena.zamkova@gmail.com>
2016-10-24 07:36:31 +0300

    Added a script and composer.json

--- script.php
+++ script.php
@@ -1,1 +1,34 @@
-<? php
+<?php
+
+require 'vendor/autoload.php';
+
+use Cocur\Slugify\Slugify;
+
+use Carbon\Carbon;
+
+
+if (empty($_GET["title"])) {
+
+    require("apology.php");
+}
+else {
+
+    $title = htmlspecialchars($_GET["title"]);
+
+    echo "<h3> Your Post Title: </h3>" . $title . "<br>";
+
+
+
+    $date = Carbon::now();
+
+    $string = $title . "-" . $date;
+
+    $slugify = new Slugify();
+
+    echo "<h3> Your Slug: </h3>" . $slugify->slugify($string);
+
+}
+
+
+
+
